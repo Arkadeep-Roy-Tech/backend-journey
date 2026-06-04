732d829b6e462a874b6cc9ab1b4ebc63a5207ac6
Arkadeep Roy <arkadeeproy02@gmail.com>
2026-06-04 17:14:01 +0530

    Day 01: wrote first Python file with vairables and print

---
+++ day01.py
@@ -0,0 +1,7 @@
+print("Day 1 complete. I wrote my first Python file.")
+
+days_completed = 1
+days_remaining = 75 - days_completed
+
+print("Days completed:", days_completed)
+print("Days remaining:", days_remaining)
