4c416de665143d3c5723eb7d94e0d0d9db7b1f53
Arkadeep Roy <arkadeeproy02@gmail.com>
2026-06-05 00:31:50 +0530

    Day 02: variables, data types, string operations, math and booleans

---
+++ day02.py
@@ -0,0 +1,30 @@
+username = "Arkadeep"
+age = 27
+height = 5.11
+is_employed = False
+
+print(username)
+print(age)
+print(height)
+print(is_employed)
+
+print(type(username))
+print(type(age))
+print(type(height))
+print(type(is_employed))
+
+print(username.upper())
+print(username.lower())
+print(len(username))
+
+print(age + 10)
+print(age - 5)
+print(age * 2)
+print(age / 4)
+print(age % 4)
+print(age ** 2)
+
+print(age > 18)
+print(age == 30)
+print(age != 30)
+print(is_employed == False)
