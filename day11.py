679e9b755d5d16fa93670a52cafc0a4d0fdbb170
Arkadeep Roy <arkadeeproy02@gmail.com>
2026-06-15 23:01:29 +0530

    Day 11: list comprehensions (transform and filter)

---
+++ day11.py
@@ -0,0 +1,9 @@
+# numbers = [1,2,3,4,5,6]
+# evens = [n for n in numbers if n % 2 == 0]
+# print(evens)
+
+numbers = [1,2,3,4,5,6]
+doubled = [n * 2 for n in numbers]
+evens = [n for n in numbers if n % 2 == 0]
+print(doubled)
+print(evens)
