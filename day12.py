25dba242710702f5e6fd96370e2ada21f677ded5
Arkadeep Roy <arkadeeproy02@gmail.com>
2026-06-22 21:54:17 +0530

    Add string methods: strip, split, join

---
+++ day12.py
@@ -0,0 +1,12 @@
+# name = "  inventory   "
+# clean_name = name.strip()
+# print(clean_name)
+
+# raw_colors = "red|green|blue"
+# colors = raw_colors.split("|")
+# print(colors)
+
+year_month_day = ["2026","06","22"]
+formatted_date = "-".join(year_month_day)
+
+print(formatted_date)
